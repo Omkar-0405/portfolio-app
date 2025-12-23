79dcd83faa266bdca21cb66618780db2a3558fe7
Omkar Bhambure <omkarb4501@gmail.com>
2025-12-24 00:39:11 +0530

    removed unwanted code and files

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -47,9 +47,7 @@ export default function DashboardLayout({ children }) {
           </div>
         </main>
       </div>
-      {/* <main className="flex-1">
-        <div className="max-w-7xl mx-auto px-6 py-6">{children}</div>
-      </main> */}
+     
     </div>
   );
 }
