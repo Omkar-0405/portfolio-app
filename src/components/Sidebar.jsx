a2c456e41a46b0acac9628a8e6a751019438ff39
Omkar Bhambure <omkarb4501@gmail.com>
2025-12-23 23:09:22 +0530

    sidebar position sticky

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,28 +7,30 @@ const navItems = [
 
 export default function Sidebar() {
   return (
-    <aside className="w-64 bg-white border-r min-h-screen px-4 py-6">
-      <div className="text-xl font-semibold mb-8">
-        Porfolio <span className="text-green-600">premium</span>
-      </div>
+    <aside className="w-64 bg-white border-r min-h-screen px-4 pb-6">
+      <div className="sticky top-0">
+        <div className="text-xl font-semibold pt-6 mb-8 ">
+          Porfolio <span className="text-green-600">premium</span>
+        </div>
 
-      <nav className="space-y-1 text-sm">
-        {navItems.map((item) => (
-          <NavLink
-            key={item.label}
-            to={item.path}
-            className={({ isActive }) =>
-              `block px-3 py-2 rounded-md ${
-                isActive
-                  ? "bg-gray-100 font-medium text-gray-900"
-                  : "text-gray-600 hover:bg-gray-50"
-              }`
-            }
-          >
-            {item.label}
-          </NavLink>
-        ))}
-      </nav>
+        <nav className="space-y-1 text-sm">
+          {navItems.map((item) => (
+            <NavLink
+              key={item.label}
+              to={item.path}
+              className={({ isActive }) =>
+                `block px-3 py-2 rounded-md ${
+                  isActive
+                    ? "bg-gray-100 font-medium text-gray-900"
+                    : "text-gray-600 hover:bg-gray-50"
+                }`
+              }
+            >
+              {item.label}
+            </NavLink>
+          ))}
+        </nav>
+      </div>
     </aside>
   );
 }
